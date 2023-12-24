04a58d9f4fa14d8042f9d8b01f6d77138e0f6b7d
KRISHRAJBHANDARI <krishrajbhandari10@gmail.com>
2023-12-24 16:36:34 +0545

    Completed Admin Pannel

---
+++ client/src/components/Prices.js
@@ -0,0 +1,32 @@
+export const Prices = [
+    {
+        _id: 0,
+        name: "Rs0 to 599",
+        array: [0, 599],
+    },
+    {
+        _id: 1,
+        name: "Rs600 to 999",
+        array: [600, 999],
+    },
+    {
+        _id: 2,
+        name: "Rs1600 to 1999",
+        array: [1600, 1999],
+    },
+    {
+        _id: 3,
+        name: "Rs2000 to 2999",
+        array: [2000, 2999],
+    },
+    {
+        _id: 4,
+        name: "Rs3000 to 3999",
+        array: [3000, 3999],
+    },
+    {
+        _id: 5,
+        name: "Rs4000 or more",
+        array: [4000, 9999],
+    },
+];
--- client/src/components/layout/AdminMenu.js
+++ client/src/components/layout/AdminMenu.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+
 const AdminMenu = () => {
     return (
         <>
@@ -10,6 +11,8 @@ const AdminMenu = () => {
                         Create Category</NavLink>
                     <NavLink to="/dashboard/admin/create-product" className="list-group-item list-group-item-action">
                         Create Product</NavLink>
+                    <NavLink to="/dashboard/admin/products" className="list-group-item list-group-item-action">
+                        Products</NavLink>
                     <NavLink to="/dashboard/admin/users" className="list-group-item list-group-item-action">
                         Users</NavLink>
                 </div>
